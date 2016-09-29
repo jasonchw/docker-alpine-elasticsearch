072db3f44c3962fb3067929499a502233b33e0ce
Jason Chan <jasonchanhw@gmail.com>
2016-09-29 15:49:30 -0400

    Added ES_PUB_IP.

--- start-elasticsearch.sh
+++ start-elasticsearch.sh
@@ -7,6 +7,10 @@ if [ -z ${ES_NODE_NAME} ]; then
     export ES_NODE_NAME=$(hostname -f)
 fi
 
+if [ "X${ES_PUB_IP}" == "X" ]; then
+    export ES_PUB_IP=$(ip -o -4 add | egrep '16|24' | head -n1 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+fi
+
 set -- gosu elasticsearch ${ES_BIN}
 
 sleep 5
