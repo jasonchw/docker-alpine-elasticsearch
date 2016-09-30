3e1e3d909807fd3d2a5b07fa887d6b5ad5462609
Jason Chan <jasonchanhw@gmail.com>
2016-09-30 14:08:46 -0400

    Renamed ES_PUB_IP to ES_PUB_HOST. Revised to change the owner of /opt/elasticsearch/.

--- start-elasticsearch.sh
+++ start-elasticsearch.sh
@@ -7,14 +7,16 @@ if [ -z ${ES_NODE_NAME} ]; then
     export ES_NODE_NAME=$(hostname -f)
 fi
 
-if [ "X${ES_PUB_IP}" == "X" ]; then
-    export ES_PUB_IP=$(ip -o -4 add | egrep '16|24' | head -n1 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+if [ "X${ES_PUB_HOST}" == "X" ]; then
+    export ES_PUB_HOST=$(ip -o -4 add | egrep '16|24' | head -n1 | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
 fi
 
 set -- gosu elasticsearch ${ES_BIN}
 
 sleep 5
 consul-template -consul localhost:8500 -once -template "/etc/consul-templates/elasticsearch.yml.ctmpl:/opt/elasticsearch/config/elasticsearch.yml"
+
+chown -R elasticsearch:elasticsearch /opt/elasticsearch/
 
 exec "$@"
 
